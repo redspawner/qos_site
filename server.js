74da9403aa5f710cb26147f2d69c2e06fafc512e
redspawner <165516917+redspawner@users.noreply.github.com>
2025-09-20 03:09:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,12 @@ const mailOptions = {
 app.use((req, res) => res.status(404).send('404: Not Found'));
 
 // Start
+const PORT = process.env.PORT;
+if (!PORT) {
+  console.error("❌ No PORT defined in environment!");
+  process.exit(1);
+}
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running at http://0.0.0.0:${PORT}`);
 });
